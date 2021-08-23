ad986f6009b5766c037c8d006083c3d6de109e37
vineetpatel7 <mailtovineetpatel@gmail.com>
2021-08-23 11:49:25 +0530

    Added : perform if-else condition

--- src/addition/ControllStatement.java
+++ src/addition/ControllStatement.java
@@ -1,11 +1,18 @@
 package addition;
 // Welcome message in master
 // Initialize data and push it in Initial branch
-// Perform the if-else condition
+// Perform the if-else condition and push itElse branch
 public class ControllStatement {
 public static void main(String[] args) {
 	System.out.println("Welcome Message");
-	int value1 = 10;
+	int value1 = 30;
 	int value2 = 20;
+
+	if(value1 > value2) {
+	    System.out.println("Greater Value : "+value1);
+	}
+	else {
+		System.out.println("Greater Value : "+value2);
+	}
 }
 }
