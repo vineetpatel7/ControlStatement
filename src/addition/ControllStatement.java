b0ab2b99ed418c23d5c116425a2e2af721e476bb
vineetpatel7 <mailtovineetpatel@gmail.com>
2021-08-23 11:31:53 +0530

    Added : initialize data

--- src/addition/ControllStatement.java
+++ src/addition/ControllStatement.java
@@ -1,9 +1,11 @@
 package addition;
 // Welcome message in master
-// Initial data and push it in Initial branch
+// Initialize data and push it in Initial branch
 // Perform the if-else condition
 public class ControllStatement {
 public static void main(String[] args) {
 	System.out.println("Welcome Message");
+	int value1 = 10;
+	int value2 = 20;
 }
 }
